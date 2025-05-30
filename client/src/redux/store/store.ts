de4375093e3b1535741e059dd509ee421eb8fe4a
Itzgregory <gregorychuks0@gmail.com>
2025-05-30 23:59:35 +0100

    full deployment

--- client/src/redux/store/store.ts
+++ client/src/redux/store/store.ts
@@ -1,7 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../slices/user/authSlice";
-import jobReducer from "../slices/jobs/jobSlice";
-import profileReducer from '../slices/user/profileSlice';
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
@@ -16,8 +14,6 @@ const persistedAuthReducer = persistReducer(authPersistConfig, authReducer);
 export const store = configureStore({
   reducer: {
     auth: persistedAuthReducer,
-    job: jobReducer,
-    profile: profileReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
